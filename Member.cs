c5ab42ee4f776c3378b00f72a49aeecbb6051ea0
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-03-02 15:33:57 -0600

    phase 1

---
+++ Member.cs
@@ -0,0 +1,12 @@
+using System;
+
+    namespace Heist 
+    {
+        class Member
+        {
+            public string name { get; set; }
+            public int skill { get; set; }
+            public double courage { get; set; }
+            
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace Heist
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             Console.WriteLine("Plan your heist!");
-        }
+            Console.WriteLine("Enter a name.");
+            string teamMateName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"How skilled is {teamMateName}? 1-10");
+            int skillValue = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"How courageous is {teamMateName}? 0.0-2.0");
+            double courageRating = double.Parse(Console.ReadLine());
+            
+            Console.Clear();
+            Console.WriteLine($"{teamMateName}");
+            Console.WriteLine($"{skillValue}/10 skill");
+            Console.WriteLine($"{courageRating}/2.0 courage");
+            }
+
+            
     }
 }
